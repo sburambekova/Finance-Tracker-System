16ca692a7f91a8ef5b81b954dffe8d965d1a52b3
mazhit-a <alibek.abilmazhit20032@gmail.com>
2025-03-02 03:55:08 +0500

    added urls handling

--- financetracker/finance/views.py
+++ financetracker/finance/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def project_list(request):
+    return render(request, 'finance/project-list.html')
+
+def project_detail(request, project_slug):
+    return render(request, 'finance/project-detail.html')
+
+
 # Create your views here.
